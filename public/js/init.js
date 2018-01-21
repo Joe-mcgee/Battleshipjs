59a2c86e6b31dca245b56c110d59c244aae29857
Taylor <thulsmans.2994@me.com>
2018-01-21 05:59:20 +0000

    added base styles for new game frame

--- public/js/init.js
+++ public/js/init.js
@@ -1,10 +1,20 @@
-var divCheck = $('<div/>', {
-  'id': 'hellothere'
-})
 
 $(document).ready(function() {
-  divCheck.append('body')
+  let newGame = $('<div/>', {
+    'id': 'newGame'
+  });
+  $(newGame).css({ 'grid-column': '1', 'display': 'grid', 'grid-template-rows': '1fr 1fr' });
+  let newButton = $('<button/>', {
+    'id': 'new'
+  });
+  $(newButton).css({'grid-row': '1', 'height':'100%', 'width':'100%', 'display':'flex', 'align-items':'center', 'justify-content':'center'});
+  newButton.append(document.createTextNode('New Game'))
+  newGame.append(newButton);
+
+  $('.main-box').append(newGame);
 });
+
+
 
 $(document).ready(function() {
   console.log('working')
