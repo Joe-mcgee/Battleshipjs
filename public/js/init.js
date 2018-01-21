7437d945f0308a905d13b7b5f335ca5e1d6932f3
Taylor <thulsmans.2994@me.com>
2018-01-21 19:32:25 +0000

    fixing init.js from corrupted repo

--- public/js/init.js
+++ public/js/init.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
   let newGame = $('<div/>', {
     'id': 'newGame'
   });
+  $(newGame).css({'display': 'grid', 'grid-template-rows': '1fr 1fr'})
+
+  let newButton = $('<button/>', {
+    'id': 'new'
+  })
 
   $(newButton).css({ 'grid-row': '1', 'height': '100%', 'width': '100%', 'display': 'flex', 'align-items': 'center', 'justify-content': 'center' });
   newButton.append(document.createTextNode('New Game'));
