ecdadc8f91680566a1e88da46b397c385ee50716
Taylor <thulsmans.2994@me.com>
2018-01-25 16:24:37 +0000

    comments to help navigate routes

--- app.js
+++ app.js
@@ -5,37 +5,49 @@ const bodyParser = require('body-parser');
 // set static directory
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+
+
+
+
+
 //use ejs templating engine
 app.set('view engine', 'ejs');
-//home route
+//home route, starts game and inits options
 app.get('/', (req, res) => {
   res.render('home');
 });
 
-//
+// renders add ships for player 1
 app.get('/new', (req, res) => {
   let templateVars = {player: 'player 1',
                       url: '/addPlayerone'};
   res.render('new', templateVars);
 });
 
+//posts options and init to server
 app.post('/new', (req, res) => {
   let templateVars = {player: 'player 1',
                       url: '/addPlayerone'}
   res.render('new', templateVars);
 });
 
+// posts player ones ships to server
 app.post('/addPlayerone', (req, res) => {
 console.log(req.body);
 res.redirect('newp2');
 
 });
 
+// posts player twos ships to server, initializes the pass screen
 app.post('/addPlayerTwo', (req, res) => {
   console.log(req.body)
   res.redirect('inter1')
 })
 
+// renders player 2's ship placement
 app.get('/newp2', (req, res) => {
   let templateVars = {
     player: 'player 2',
@@ -45,15 +57,18 @@ app.get('/newp2', (req, res) => {
 });
 
 
+// pass screen between player two and player one
 app.get('/inter1', (req, res) => {
   let templateVars = {player: 'player1',
                       url: '/player1turn'}
   res.render('inter', templateVars)
 })
 
+// posts player ones move to server
 app.post('/player1turn', (req, res) => {
-  res.render('player1turn')
+  res.redirect('player1turn')
 })
+
 
 app.get('/player1turn', (req, res) => {
   let templateVars = {player: 'player 1',
