27ba9223e34aa7f95a46f24ed02d860e309c55b1
Taylor <thulsmans.2994@me.com>
2018-01-31 22:51:13 +0000

    adds a ship status foreach player

--- public/js/ship-list.js
+++ public/js/ship-list.js
@@ -1,39 +1,50 @@
 const SHIPNAMES = ['Carrier', 'Battleship', 'Cruiser', 'Submarine', 'Destroyer']
 
-function draw shipList(fleet) {
+function shipList(fleet) {
   let isHit = new RegExp('^X')
   let list = $('<section/>', {
     'class': 'ship-list'
   })
-  $(list).css({'display':'grid', 'grid-template-rows':'1'})
-  $(list).css('height', function() {
-    var height = $(window).width() / 2;
-    var cell = height / 11;
-    return cell;
-  })
+  $(list).css({'background-color': 'cadetblue'})
+
 
   fleet.forEach((ship, i) => {
     let label = $('<p/>', {
-    'class':'label',
-  })
-  $(label).append(document.createTextNode(SHIPNAMES[i]))
-  $(list).append(label)
+      'class': 'label',
+    })
+    $(label).append(document.createTextNode(SHIPNAMES[i]))
+    $(list).append(label)
     ship.forEach((coord) => {
       let cell = $('<div/>', {
-
+          'class':'cellLog'
+      })
+      $(cell).css('width', function() {
+        var height = $(window).width() / 2;
+        var cell = height / 11;
+        return cell;
+      })
+      $(cell).css('height', function() {
+        var height = $(window).width() / 2;
+        var cell = height / 11;
+        return cell;
       })
 
-      if(isHit.test(coord)) {
-
+      if (isHit.test(coord)) {
+        $(cell).css({ 'background-color': 'red' })
+      } else {
+        $(cell).css({ 'background-color': 'white' });
       }
+      $(label).append(cell)
     })
+    $(list).append(label)
 
   });
-
+  return list
 }
 
 
 
 $(document).ready(function() {
-
+  $('body').append(shipList(fleet))
 })
+
